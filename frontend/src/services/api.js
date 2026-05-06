d80a45b9d02968b6a2a1928b854cd332aba85740
Samoh <67761385+Samohtechs@users.noreply.github.com>
2026-05-06 21:25:30 +0300

    Updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://nipbackend.udomcyberclub.org",
   timeout: 10000,
 });
 
